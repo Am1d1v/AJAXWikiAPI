b699fafdfc721ae1b460729a4a6045ad47e39a95
Am1d1v <dimadiman1adima@gmail.com>
2024-02-10 13:15:10 +0300

    Search Request

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 
 
-const url = '';
+const url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=javascript';
 const btn = document.querySelector('.btn');
 const output = document.querySelector('.output')
 const inputVal = document.querySelector('.val');
 
 
+//Event Listeners
+//Fetch Data
+btn.addEventListener('click', async() => {
+    const fetchData = await fetch(url);
+    const JSONData = await fetchData.json();
+    
+    search(JSONData);
+});
 
+// Make a search request
+function search(data){
+    console.log(data.query.search);
+}
 
